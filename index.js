f39af7fa301408c13f2c698ae758a3562f7bd192
MarcioAlex-x <marcioalex586@gmail.com>
2024-11-30 16:26:41 -0300

    metodo post

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mysql = require('mysql2')
 
 const app = express()
 
+app.use(express.urlencoded({extends:true}))
+app.use(express.json())
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
@@ -11,17 +14,40 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
+app.get('/create-post',(req, res)=>{
+    res.render('form-post')
+})
+
+app.post('/add-post',(req, res)=>{
+    const titulo = req.body.titulo
+    const subtitulo = req.body.subtitulo
+    const artigo = req.body.artigo
+    const data_publicacao = req.body.data_publicacao
+    const autor = req.body.autor
+    const fonte = req.body.fonte
+
+    const sql = `INSERT INTO noticias (titulo, subtitulo, artigo, data_publicacao, autor, fonte) VALUES ('${titulo}','${subtitulo}','${artigo}','${data_publicacao}','${autor}','${fonte}')`
+
+    conn.query(sql,(err)=>{
+        console.log(err)
+    })
+
+    res.redirect('/')
+})
+
 const conn = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'root',
-    database:'noticias'
+    database:'noticia'
 })
 
 conn.connect((err)=>{
     if(err){
         console.log(err)
     }else{
-        app.listen(3000)
+        app.listen(3000,()=>{
+            console.log('http://localhost:3000')
+        })
     }
 })
